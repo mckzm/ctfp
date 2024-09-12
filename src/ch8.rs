b664b85e3d56d49f3572889eff2fb9d77c0ed147
mckzm <134839822+mckzm@users.noreply.github.com>
2024-09-13 02:56:16 +0900

    impl Bifunctor for pair and Result types

--- src/ch8.rs
+++ src/ch8.rs
@@ -64,8 +64,38 @@ impl<T, U> Bifunctor for Either<T, U> {
     }
 }
 
+impl<T, U> Bifunctor for (T, U) {
+    type InnerFirst = T;
+    type InnerSecond = U;
+    type OuterTarget<V, W> = (V, W);
+
+    fn first<V, F: Fn(T) -> V>(self, f: F) -> (V, U) {
+        (f(self.0), self.1)
+    }
+
+    fn second<W, G: Fn(U) -> W>(self, g: G) -> (T, W) {
+        (self.0, g(self.1))
+    }
+}
+
+impl<T, E> Bifunctor for Result<T, E> {
+    type InnerFirst = T;
+    type InnerSecond = E;
+    type OuterTarget<V, W> = Result<V, W>;
+
+    fn first<V, F: Fn(T) -> V>(self, f: F) -> Result<V, E> {
+        self.map(f)
+    }
+
+    fn second<W, G: Fn(E) -> W>(self, g: G) -> Result<T, W> {
+        self.map_err(g)
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use std::num::ParseIntError;
+
     use super::*;
 
     #[test]
@@ -83,5 +113,42 @@ mod tests {
         let right = Either::Right(42u32);
         let right_up = right.bimap(incr_to_u32, incr_to_u64);
         assert_eq!(right_up, Either::Right(43u64));
+    }
+
+    #[test]
+    fn test_pair_bifunctor() {
+        let split = |s: &str| {
+            s.split_ascii_whitespace()
+                .map(|s| s.to_string())
+                .collect::<Vec<String>>()
+        };
+        let incr_pair = |(a, b): (u32, u32)| ((a + 1) as u64, (b + 1) as u64);
+        let v = vec![
+            "First".to_string(),
+            "second".to_string(),
+            "third".to_string(),
+        ];
+
+        let pair = ("First second third", (0, 1));
+        let uplifted = pair.bimap(split, incr_pair);
+        assert_eq!(uplifted, (v, (1u64, 2u64)));
+    }
+
+    #[test]
+    fn test_result_bifunctor() {
+        let make_pair = |n: u32| (n, n + 1);
+        let log = |e: ParseIntError| e.to_string() + " - with uplift";
+
+        let ok: Result<u32, _> = "42".parse();
+        let err = "Hello, World!".parse::<u32>();
+
+        let ok = ok.bimap(make_pair, log);
+        let err = err.bimap(make_pair, log);
+
+        assert_eq!(ok, Ok((42u32, 43u32)));
+        assert_eq!(
+            err,
+            Err("invalid digit found in string - with uplift".to_string())
+        );
     }
 }
